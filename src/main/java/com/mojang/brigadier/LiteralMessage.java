96c16cd5e8e16c1da14f6d697960a6fcb5d8607e
Intellegibile <78700221+Intellegibile@users.noreply.github.com>
2021-07-10 10:45:50 +0200

    LiteralMessage 1.0
    Classe completa

--- src/main/java/com/mojang/brigadier/LiteralMessage.java
+++ src/main/java/com/mojang/brigadier/LiteralMessage.java
@@ -3,18 +3,33 @@
 
 package com.mojang.brigadier;
 
+/**
+ * Questa classe implementa Message
+ * Definisce un messaggio, viene usata solo
+ * per ritornare una stringa
+ */
 public class LiteralMessage implements Message {
-    private final String string;
+    private final String string; //La stringa che contiene il messaggio
 
+    /**
+     * Il costruttore che inizializza la stringa del messaggio
+     * @param string valore da inizializzare a string
+     */
     public LiteralMessage(final String string) {
         this.string = string;
     }
 
+    /**
+     * @return La stringa
+     */
     @Override
     public String getString() {
         return string;
     }
 
+    /**
+     * @return Ritorna la stringa
+     */
     @Override
     public String toString() {
         return string;
